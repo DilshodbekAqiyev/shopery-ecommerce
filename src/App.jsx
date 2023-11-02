c74052fbf6ca7c96bbf82348697c3c849ce671f8
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-11-02 22:34:45 +0500

    mock products data added and shop page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Button } from './components/ui/button'
+import { Button } from './components/ui/button';
 
 export default function App() {
   return (
     <div className='container my-3'>
       <Button>Click Me</Button>
     </div>
-  )
+  );
 }
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,5 +1,13 @@
 function Shop() {
-  return <div>Shop</div>;
+  return (
+    <div className='max-w-[1320px] bg-slate-100'>
+      <div>Top</div>
+      <div className='flex justify-between'>
+        <div className='w-[24%] bg-red-100'>Filter</div>
+        <div className='w-[74%] bg-gray-400'>Products</div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
