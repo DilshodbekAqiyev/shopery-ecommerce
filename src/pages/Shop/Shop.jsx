c8c189b5bbe42c9f54801d6884154e8560abb5e7
Dilshodbek <129289721+DilshodbekAqiyev@users.noreply.github.com>
2023-11-02 22:42:54 +0500

    Dilshodbek (#3)
    * shop page created
    
    * mock products data added and shop page created
    
    * mock products data added and shop page created

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,13 @@
+function Shop() {
+  return (
+    <div className='max-w-[1320px] bg-slate-100'>
+      <div>Top</div>
+      <div className='flex justify-between'>
+        <div className='w-[24%] bg-red-100'>Filter</div>
+        <div className='w-[74%] bg-gray-400'>Products</div>
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
