548b6cf81762c603d109c6619f177c89b33cfc87
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-11-03 01:00:43 +0500

    Samandar (#10)
    * Blog page started
    
    * filter-icon added
    
    * blog
    
    * home create new
    
    * tek
    
    * tek3
    
    * ui

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,5 +1,42 @@
+
+import BlogCard from "./BlogCard";
+import BlogSearch from "./Search";
+import BlogFilterButton from "./FilterButton";
+import BlogTopCategories from "./TopCategories";
+import BlogPopularTag from "./PopularTag";
+
+// const cardData = [];
+
 const Blog = () => {
-   return <div>Blog</div>;
-};
+   return (
+      <div className="blog flex gap-[24px] mt-[34px]">
+         <div className="left w-[424px]">
+            <BlogFilterButton />
+            <BlogSearch />
+            <BlogTopCategories />
+            <BlogPopularTag />
+         </div>
+         <div className="right w-[872px]">
+            <div className="rightTop flex item-center justify-between ">
+               <div className="flex gap-[8px] item-center">
+                  <h3>Sort by</h3>
+                  <select id="sortBy" className="py-[10px] px-[16px]">
+                     <option value="Latest">Latest</option>
+                     <option value="L">Latest</option>
+                  </select>
+               </div>
+               <div className="">
+                  <h3>
+                     <span className="font-bold pr-1">52</span> Result Found
+                  </h3>
+               </div>
+            </div>
+            <div className="BlogCards">
+               <BlogCard />
+            </div>
+         </div>
+      </div>
+   );
+
 
 export default Blog;
