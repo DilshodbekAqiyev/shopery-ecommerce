f0241b3aded9038bcf21e8ca0a99e03e75b514b7
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-11-02 23:41:20 +0500

    shop page top start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Button } from './components/ui/button';
+import Shop from './pages/Shop/Shop';
 
 export default function App() {
   return (
     <div className='container my-3'>
       <Button>Click Me</Button>
+      <Shop />
     </div>
   );
 }
