be3a1a70216fe4924fce91f3cc8b2b7b2745f318
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-11-02 22:46:49 +0500

    Samandar (#4)
    * Blog page started
    
    * filter-icon added
    
    * blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Button } from './components/ui/button';
+
+import { Button } from "./components/ui/button";
+import Blog from "./pages/Home/Blog/Blog";
 
 export default function App() {
-  return (
-    <div className='container my-3'>
-      <Button>Click Me</Button>
-    </div>
-  );
-}
+   return (
+      <div className="container my-3">
+         <Button>Click Me</Button>
+         <Blog />
+      </div>
+   );
+
