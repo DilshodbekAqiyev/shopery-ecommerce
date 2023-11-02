90df3f365a857789554404c79d95172ffaa0b9c2
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-11-03 02:07:06 +0500

    added:description Tab

--- src/pages/ProductDetails/__mock__/detailsData.jsx
+++ src/pages/ProductDetails/__mock__/detailsData.jsx
@@ -1,19 +1,32 @@
-import { FaCheckCircle } from "react-icons/fa";
+import { AiFillCheckCircle } from "react-icons/ai";
 export const CheckData = [
   {
-    icon: <FaCheckCircle className="text-primary" />,
+    icon: <AiFillCheckCircle className="text-primary text-lg" />,
     title: "100 g of fresh leaves provides.",
   },
   {
-    icon: <FaCheckCircle className="text-primary" />,
+    icon: <AiFillCheckCircle className="text-primary text-lg" />,
     title: "Aliquam ac est at augue volutpat elementum.",
   },
   {
-    icon: <FaCheckCircle className="text-primary" />,
+    icon: <AiFillCheckCircle className="text-primary text-lg" />,
     title: "Quisque nec enim eget sapien molestie.",
   },
   {
-    icon: <FaCheckCircle className="text-primary" />,
+    icon: <AiFillCheckCircle className="text-primary text-lg" />,
     title: "Proin convallis odio volutpat finibus posuere.",
   },
 ];
+
+export const featureData = [
+  {
+    img:'/assets/icons/product-details-icon/price-tag.svg',
+    title:'64% Discount',
+    description:'Save your 64% money with us'
+  },
+  {
+    img:'/assets/icons/product-details-icon/leaf.svg',
+    title:'64% Discount',
+    description:'Save your 64% money with us'
+  },
+]
--- src/pages/ProductDetails/components/Tabs.jsx
+++ src/pages/ProductDetails/components/Tabs.jsx
@@ -4,33 +4,36 @@ import {
   TabsList,
   TabsTrigger,
 } from "../../../components/ui/tabs";
-import { CheckData } from "../__mock__/detailsData";
+import { CheckData, featureData } from "../__mock__/detailsData";
 
 function TabBar() {
   return (
     <Tabs defaultValue="description" className="w-full bg-white">
-      <TabsList className="bg-white">
+      <TabsList className="bg-white border-b-2 border-b-gray-300 w-full rounded-none">
         <TabsTrigger
           value="description"
-          className="data-[state=active]:bg-white data-[state=active]:shadow-none rounded-none border-white border-2 data-[state=active]:border-b-primary"
+          className="data-[state=active]:bg-white data-[state=active]:shadow-none pb-3 rounded-none border-2 border-white border-b-gray-300 data-[state=active]:border-b-primary"
         >
           Descriptions
         </TabsTrigger>
         <TabsTrigger
           value="additionInfo"
-          className="data-[state=active]:bg-white data-[state=active]:shadow-none rounded-none border-white border-2 data-[state=active]:border-b-primary"
+          className="data-[state=active]:bg-white data-[state=active]:shadow-none pb-3 rounded-none border-2 border-white border-b-gray-300 data-[state=active]:border-b-primary"
         >
           Additional Information
         </TabsTrigger>
         <TabsTrigger
           value="customerFeedback"
-          className="data-[state=active]:bg-white data-[state=active]:shadow-none rounded-none border-white border-2 data-[state=active]:border-b-primary"
+          className="data-[state=active]:bg-white data-[state=active]:shadow-none pb-3 rounded-none border-2 border-white border-b-gray-300 data-[state=active]:border-b-primary"
         >
           Customer Feedback
         </TabsTrigger>
       </TabsList>
-      <TabsContent value="description" className="text-textSmall">
-        <>
+      <TabsContent
+        value="description"
+        className="text-textSmall text-grays-gray500 mt-8 flex justify-between gap-[136px]"
+      >
+        <div className="flex flex-col gap-5 items-start w-[660px]">
           <p>
             Sed commodo aliquam dui ac porta. Fusce ipsum felis, imperdiet at
             posuere ac, viverra at mauris. Maecenas tincidunt ligula a sem
@@ -48,7 +51,7 @@ function TabBar() {
             velit, pellentesque tristique neque mi eget nulla. Proin luctus
             elementum neque et pharetra.
           </p>
-          <>
+          <div className="flex flex-col gap-3">
             {CheckData.map((data) => {
               return (
                 <div key={data.title} className="flex items-center gap-2">
@@ -57,16 +60,33 @@ function TabBar() {
                 </div>
               );
             })}
-          </>
+          </div>
           <p>
             Cras et diam maximus, accumsan sapien et, sollicitudin velit. Nulla
             blandit eros non turpis lobortis iaculis at ut massa.
           </p>
-        </>
-        <></>
+        </div>
+        <div className="flex flex-col items-start gap-5 w-[536px]">
+        <div className=" bg-greenGrays-greenGray700 w-full h-[320px] flex justify-center items-center rounded-lg">Video</div>
+        <div className="border-2 border-grays-gray100 px-5 py-6 flex justify-between w-full rounded-lg">
+          {featureData.map(({title,img,description}) => {
+            return <div key={title} className="flex items-center gap-3">
+              <img src={img} alt="featureImg" loading="lazy"/>
+              <div className="flex flex-col gap-1">
+                <h4 className="font-[500] text-grays-gray900 text-textSmall">{title}</h4>
+                <p className="text-[13px]">{description}</p>
+              </div>
+            </div>
+          })}
+        </div>
+        </div>
       </TabsContent>
-      <TabsContent value="additionInfo">Additional Information</TabsContent>
-      <TabsContent value="customerFeedback">Customer Feedback</TabsContent>
+      <TabsContent value="additionInfo" className="mt-8">
+        Additional Information
+      </TabsContent>
+      <TabsContent value="customerFeedback" className="mt-8">
+        Customer Feedback
+      </TabsContent>
     </Tabs>
   );
 }
