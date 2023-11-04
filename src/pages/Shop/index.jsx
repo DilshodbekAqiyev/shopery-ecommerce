29895a5919613fbc0ce754d9b151473011e51b26
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-11-04 18:47:41 +0500

    commit

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
 import PopularTags from './PopularTags'
 import AllCategoriesComponent from './AllCategories'
-import { Button } from '../../components/ui/button'
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '../../components/ui/select'
 import { Accordion } from '../../components/ui/accordion'
 import SliderComponent from './SliderComponent'
 import Rating from './Rating'
+import TopComponent from './TopComponent'
 
 function Shop() {
   // eslint-disable-next-line no-unused-vars
@@ -26,26 +25,7 @@ function Shop() {
     <div className="max-w-[1320px] mx-auto pt-8 transition-all ease-linear duration-500">
       <div className="pb-6 flex justify-between items-center">
         <div className="flex justify-between items-center gap-x-60">
-          <Button className="rounded-[43px] font-semibold text-white text-[14px] px-6 py-3">
-            Filter <img src="/assets/icons/filter.svg" alt="filter icon" className="inline-block ml-3 w-6 h-6" />
-          </Button>
-          <div className="flex items-center gap-x-2">
-            Sort by:{'  '}
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Sort by Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="latest">Latest</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
+          <TopComponent />
         </div>
         <div className="text-grays-gray600 font-normal">
           <span className="text-grays-gray900 inline-block font-bold text-base">52</span> Results Found
