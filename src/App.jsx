bc586fdda9e9a619cbb57fcc8a9597c3c7d350a6
Asadbek <asavbatov@gmail.com>
2023-11-04 22:03:13 +0500

    Translation and Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import {
-   createBrowserRouter,
-   RouterProvider,
-   createRoutesFromElements,
-   Route
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
 
-import RootLayout from "./components/layouts";
+import RootLayout from './components/layouts'
 
-import Blog from "./pages/Blog/Blog";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-
+import Blog from './pages/Blog/Blog'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
 
 export default function App() {
-   const routes = createBrowserRouter(
-      createRoutesFromElements(
-         <Route path='/' element={<RootLayout />}>
-            <Route path='blog' element={<Blog />} />
-            <Route path='products' element={<ProductDetails />} />
-         </Route>
-      )
-   )
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route path="blog" element={<Blog />} />
+        <Route path="products" element={<ProductDetails />} />
+      </Route>
+    )
+  )
 
-   return <RouterProvider router={routes} />
+  return <RouterProvider router={routes} />
 }
