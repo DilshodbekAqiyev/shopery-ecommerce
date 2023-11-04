944ad60f439fe099a67f3d5e51f98b5b86ce6dc6
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-11-05 01:27:29 +0500

    added:Slider images

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,13 @@
+import SwiperContent from './components/SwiperContent'
 import TabBar from './components/Tabs'
 
 function ProductDetails() {
   return (
     <div className=" max-w-[1320px] mx-auto">
+      <div className="flex items-start gap-3 mt-[34px] mb-1">
+        <SwiperContent />
+        <div>details content</div>
+      </div>
       <TabBar />
       <h1 className="text-heading05 font-[600] text-center mb-8">Related Products</h1>
     </div>
