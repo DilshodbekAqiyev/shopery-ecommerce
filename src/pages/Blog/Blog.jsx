9df6401ec3020f18324044d37de02d6d9dc02db2
Samandar Turaev <samandarturayev217@gmail.com>
2023-11-05 00:34:59 +0500

    select added

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -5,6 +5,7 @@ import BlogTopCategories from './TopCategories'
 import BlogPopularTag from './PopularTag'
 import BlogOurGallery from './OurGallery'
 import BlogRecentlyAdded from './RecentlyAdded'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../components/ui/select'
 
 // const cardData = [];
 
@@ -27,20 +28,26 @@ const Blog = () => {
 
       <div className="right w-[872px]">
         <div className="rightTop flex item-center justify-between ">
-          <div className="flex gap-[8px] item-center">
+          <div className="flex gap-4 items-center ">
             <h3>Sort by</h3>
-            <select id="sortBy" className="py-[10px] px-[16px]">
-              <option value="Latest">Latest</option>
-              <option value="L">Latest</option>
-            </select>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
           <div className="">
             <h3>
-              <span className="font-bold pr-1">52</span> Result Found
+              <span className="font-bold pr-1">{52}</span> Result Found
             </h3>
           </div>
         </div>
-        <div className="BlogCards">
+        <div className="BlogCards mt-8">
           <BlogCard />
         </div>
       </div>
