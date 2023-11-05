bef0cde09d158b3309baedf8f8c274c0c94b8ac3
Gozal <gozalmominova6gmail.com>
2023-11-05 12:51:56 +0500

    category added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { space } from "postcss/lib/list";
 import ProductCard from "../../components/common/Cards/ProductCard";
 
 const data = {
@@ -12,21 +13,31 @@ const data = {
    statusColor: 'green'
 }
 
+const vegetablesData = [
+   "All", "Vegetables", "Fruit", "Meat & Fish", " View All"
+]
 const Home = () => {
-   return <div className="container  border">
-      <div className="flex">
-         <ProductCard {...data} />
-         <ProductCard {...data} />
-         <ProductCard {...data} />
-         <ProductCard {...data} />
+   return (<>
+      <h1 className="font-[600] text-[40px] text-center mt-[100px] mb-[24px]">Introducing Our Products</h1>
+      <div className="text-center mb-[50px]">
+         {vegetablesData.map((item, index) => {
+            return <a href="#0"><span className="hover:border-b-2 hover:text-[#00B207] text-[#808080] text-[16px] font-[500] border-[#20B526] py-[8px] px-[12px]">{item}</span> {index < vegetablesData.length - 1 ? <span className="mx-[10px] text-[#B4CCB4] font-[400]">|</span> : ''}</a>
+         })}
       </div>
-      <div className="flex">
-         <ProductCard {...data} />
-         <ProductCard {...data} />
-         <ProductCard {...data} />
-         <ProductCard {...data} />
-      </div>
-   </div>;
+      <div className="container  border">
+         <div className="flex">
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+         </div>
+         <div className="flex">
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+            <ProductCard {...data} />
+         </div>
+      </div></>)
 };
 
 export default Home;
