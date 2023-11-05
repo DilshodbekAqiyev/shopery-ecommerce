8765087993f2c85e32c81fc1911b60aa9552af7e
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-11-05 18:18:47 +0500

    fix:bug

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import { Badge } from '../../components/ui/badge'
 import SwiperContent from './components/SwiperContent'
 import TabBar from './components/Tabs'
 
@@ -7,7 +8,28 @@ function ProductDetails() {
       <div className="flex items-start gap-3 mt-[34px] mb-1">
         <SwiperContent />
         <>
-          <h1 className="text-heading05 font-[600]">Chinese Cabbage</h1>
+          <div className="flex flex-col items-start">
+            <div className="flex items-center gap-2 mb-3">
+              <h1 className="text-heading05 font-[600]">Chinese Cabbage</h1>
+              <Badge
+                className={'bg-softPrimary text-hardPrimary rounded-sm py-[2px] px-2 font-[400] hover:bg-softPrimary'}
+              >
+                In Stock
+              </Badge>
+            </div>
+            <div className="text-gray-500 flex items-center gap-3">
+              <h1>⭐⭐⭐⭐⭐ 4 Review</h1>
+              <span>•</span>
+              <div className="flex items-center gap-1">
+                <h4 className="text-black font-[500]">SKU:</h4>
+                <span>251594</span>
+              </div>
+            </div>
+            <div></div>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
         </>
       </div>
       <TabBar />
