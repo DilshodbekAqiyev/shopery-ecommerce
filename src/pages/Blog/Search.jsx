215c6ee7c5942a68442d50787a1cf082da28014d
Dilshodbek <129289721+DilshodbekAqiyev@users.noreply.github.com>
2023-11-05 09:39:57 +0500

    refactoring codes please view all (#22)
    * shop page created
    
    * mock products data added and shop page created
    
    * mock products data added and shop page created
    
    * shop page top start
    
    * fixed
    
    * installed accardion and select
    
    * componentalarga bo'lindi
    
    * commit
    
    * added products card and updated products data
    
    * changed locales file data to src/locales , refactoring blogs page codes and pulled

--- src/pages/Blog/Search.jsx
+++ src/pages/Blog/Search.jsx
@@ -1,9 +1,9 @@
-import FilterSearchIcon from '../../../public/assets/icons/blog-icons/filter-search-icon'
+import { FiSearch } from 'react-icons/fi'
 
 const BlogSearch = () => {
   return (
     <div className="search flex gap-[8px] item-center mt-[32px] mb-[28px] py-[14px] px-[16px] border rounded-[6px]  ">
-      <FilterSearchIcon />
+      <FiSearch size={24} />
       <input type="text" placeholder="Search..." className=" w-[100%] outline-none" />
     </div>
   )
