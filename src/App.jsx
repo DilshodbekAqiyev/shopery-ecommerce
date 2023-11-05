5e53bd1645eaeb872db57b6780bcffc24574ae08
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-11-05 21:40:44 +0500

    Samandar (#30)
    * Blog page started
    
    * filter-icon added
    
    * blog
    
    * home create new
    
    * tek
    
    * tek3
    
    * ui
    
    * git pull origin main
    
    * button
    
    * dsfdfds
    
    * new
    
    * Blog card satrted
    
    * tek17
    
    * card finished
    
    * container added
    
    * add tek19
    
    * select added
    
    * card addwd
    
    * img
    
    * blog components
    
    * category finished
    
    * ddd
    
    * popular tag
    
    * blog filter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route
-} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
 
 // Pages
 import Home from './pages/Home/Home'
 import Shop from './pages/Shop'
-import Blog from './pages/Blog/Blog'
+import Index from './pages/Blog'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
 import Signup from './pages/Auth/Signup'
 import Signin from './pages/Auth/Signin'
@@ -22,7 +17,7 @@ export default function App() {
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
-        <Route path="blog" element={<Blog />} />
+        <Route path="blog" element={<Index />} />
         <Route path="products" element={<ProductDetails />} />
         <Route path="shop" element={<Shop />} />
         <Route path="sign-in" element={<Signup />} />
--- src/pages/Blog/components/BlogCard.jsx
+++ src/pages/Blog/components/BlogCard.jsx
@@ -1,8 +1,11 @@
-const BlogCard = () => {
+import PropTypes from 'prop-types'
+
+const BlogCard = ({ prop }) => {
+  const { title, previewImage, id, comments } = prop
   return (
     <div className="blogCard w-[424px]">
       <div className="blogCardTop w-[424px] h-[324px] rounded-t-[8px] relative">
-        <img src={'/assets/images/fruits.jpg'} alt="img" className="rounded-t-[8px] w-[424px] h-[324px] " />
+        <img src={'/assets/images/fruits.jpg'} alt="img" className="rounded-t-[8px] max-w-[424px] h-[324px] " />
         <div className="absolute bottom-[24px] left-[24px] z-5 bg-white text-gray-900 flex flex-col items-center justify-center pt-[6px] px-[15px] pb-[10px] rounded">
           <h4 className="text-[20px] ">18</h4>
           <p className="text-[12px] text-grays-gray500">NOV</p>
@@ -26,14 +29,12 @@ const BlogCard = () => {
           <li>
             <a href="#2 " className="flex items-center gap-1">
               <img src={'/assets/icons/chatComment.svg'} alt="img" />
-              <span>{65} Comments</span>
+              <span>{comments.length} Comments</span>
             </a>
           </li>
         </ul>
-        <h4 className="text-[18px] mt-2 mb-5 font-[500]">
-          Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.
-        </h4>
-        <a href="#2" className="flex gap-[12px] text-primary text-[16px] font-[600]">
+        <h4 className="text-[18px] mt-2 mb-5 font-[500]">{title}</h4>
+        <a href="#id" className="flex gap-[12px] text-primary text-[16px] font-[600]">
           <span>Read More </span>
 
           <img src="assets/icons/ArrowRight.svg" alt="img" />
@@ -44,3 +45,7 @@ const BlogCard = () => {
 }
 
 export default BlogCard
+
+BlogCard.propTypes = {
+  prop: PropTypes.any,
+}
