fb17bb0d26fbe33f5955ac48df9e72ea578b8754
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-11-05 22:52:13 +0500

    Revert "Samandar (#30)"
    This reverts commit 5e53bd1645eaeb872db57b6780bcffc24574ae08.

--- src/pages/Blog/components/TopCategories.jsx
+++ src/pages/Blog/components/TopCategories.jsx
@@ -1,42 +1,39 @@
-import PropTypes from 'prop-types'
-
-const BlogTopCategories = ({ filterData }) => {
-  const arr = []
-  const obj = {}
-
-  filterData.forEach((data) => {
-    arr.push(data.category)
-  })
-
-  for (let i = 0; i < arr.length; i++) {
-    if (obj[arr[i]]) {
-      obj[arr[i]] = obj[arr[i]] + 1
-    } else {
-      obj[arr[i]] = 1
-    }
-  }
-
-  const categorys = Object.entries(obj)
-
+const BlogTopCategories = () => {
   return (
     <div>
       <h3 className="mb-[20px] font-bold">Top Categories</h3>
       <ul className="flex flex-col gap-4">
-        {categorys.map((category, idx) => {
-          return (
-            <li key={idx} className="flex justify-between item-center pb-[16px ]">
-              <p>{category[0]}</p>
-              <p>({category[1]})</p>
-            </li>
-          )
-        })}
+        <li className="flex justify-between item-center pb-[16px ]">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
+        <li className="flex justify-between item-center">
+          <p>Fresh Fruit</p>
+          <p>(134)</p>
+        </li>
       </ul>
     </div>
   )
 }
 
 export default BlogTopCategories
-
-BlogTopCategories.propTypes = {
-  filterData: PropTypes.array,
-}
