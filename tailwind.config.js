00ad200de3a27e3c79f8e6c77a4095bf4164ebc7
Suhrob <135635990+SuhrobAbdumalikov@users.noreply.github.com>
2023-11-05 18:55:44 +0500

    Suxrob (#26)
    * added: typography and colors
    
    * prepared:Tabs
    
    * fix:Conflicts
    
    * fix:Conflicts
    
    * fix:bug
    
    * fix:bug
    
    * added:description Tab
    
    * added:description Tab
    
    * finished:Tabs
    
    * buttuns prepared started
    
    * prepared:all btn types and size
    
    * added:Fonts
    
    * fixed:tabs bugs
    
    * added:video,reducer,types and fixed:prettier
    
    * useState changed to useReducer
    
    * fixed:conflicts
    
    * added:Slider images
    
    * remove:smth
    
    * fixed: conflicts
    
    * fix:bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -109,5 +109,5 @@ module.exports = {
     },
   },
   // eslint-disable-next-line no-undef
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 }
