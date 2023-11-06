6541a1d323d1092c56bd155d0afe5e686b41d4e4
Xudayqulov Ravshanbek <ravshanxudoyqulov57@gmail.com>
2023-11-06 09:54:13 +0500

    homega qoshildiu

--- src/pages/Home/FeaturedProducts.jsx
+++ src/pages/Home/FeaturedProducts.jsx
@@ -1,10 +1,12 @@
 import ProductCard from '../../components/common/Cards/ProductCard'
+// eslint-disable-next-line react/prop-types
 const FeaturedProducts = ({ dataFeatures }) => {
   return (
     <div className="pt-[100px] ">
       <h1 className="text-center font-[600] text-[40px] mb-[50px]">Featured Products</h1>
       <div className="flex">
         {Array.from(Array(5)).map((_) => {
+          // eslint-disable-next-line react/jsx-key
           return <ProductCard {...dataFeatures} />
         })}
       </div>
