b49820bb5313743c159174aa8220c0e7b0e469e2
Dilshodbek <129289721+DilshodbekAqiyev@users.noreply.github.com>
2023-11-07 23:16:14 +0500

    Dilshodbek (#59)
    * shop page created
    
    * mock products data added and shop page created
    
    * mock products data added and shop page created
    
    * shop page top start
    
    * fixed
    
    * installed accardion and select
    
    * componentalarga bo'lindi
    
    * commit
    
    * added products card and updated products data
    
    * changed locales file data to src/locales , refactoring blogs page codes and pulled
    
    * fixed
    
    * changed getData function
    
    * added selection colors
    
    * refactoring shop page codes
    
    * update products data
    
    * categoriyalar boyicha va rating boyicha filterlash oxirigacha tugadi
    
    * added find product from products to single product

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,28 +1,28 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import ModalDetails from './components/ModalDetails'
 import TabBar from './components/Tabs'
-// import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { instance } from '../../utils/apiRequest'
 
-function ProductDetails({ data }) {
-  // const [product, setProduct] = useState([])
-  // const { id } = useParams()
-  console.log('data::' + data)
+function ProductDetails() {
+  const { id } = useParams()
+  const [foundProduct, setFoundProduct] = useState(null)
 
-  // useEffect(() => {
-  //   const foundProduct = data?.find((item) => item.id === id)
-  //   console.log(foundProduct)
-  //   if (foundProduct) {
-  //     setProduct(foundProduct)
-  //   } else {
-  //     return
-  //   }
-  // }, [data, id])
+  console.log(foundProduct)
+
+  useEffect(() => {
+    // eslint-disable-next-line no-extra-semi
+    ;(async () => {
+      const response = await instance.get(`products/${id}`)
+      setFoundProduct(response.data)
+    })()
+  }, [id])
 
   return (
-    <div className=" max-w-[1320px] mx-auto">
+    <div className="max-w-[1320px] mx-auto">
       <div>
-        <ModalDetails />
+        <ModalDetails product={foundProduct} />
         <TabBar />
         <h1 className="text-heading05 font-[600] text-center mb-8">Related Products</h1>
       </div>
