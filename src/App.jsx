940e76b5449252a8d07d669edc7e24670e0a703b
Xudayqulov Ravshanbek <ravshanxudoyqulov57@gmail.com>
2023-11-07 10:41:48 +0500

    d

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ProductDetails from './pages/ProductDetails/ProductDetails'
 import Signup from './pages/Auth/Signup'
 import Signin from './pages/Auth/Signin'
 import PageNotFound from './components/common/PageNotFound'
-
+import Checkout from './pages/Checkout/Checkout'
 // Layouts
 import RootLayout from './components/layouts/RootLayout'
 
@@ -32,7 +32,7 @@ export default function App() {
         />
         <Route path="sign-in" element={<Signup />} />
         <Route path="sign-up" element={<Signin />} />
-
+        <Route path="checkout" element={<Checkout />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     )
