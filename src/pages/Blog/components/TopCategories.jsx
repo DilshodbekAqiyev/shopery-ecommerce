54776a8e35843847e4352ae89947cfa0bb32ecc9
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-11-07 08:30:21 +0500

    Samandar (#46)
    * Blog page started
    
    * filter-icon added
    
    * blog
    
    * home create new
    
    * tek
    
    * tek3
    
    * ui
    
    * git pull origin main
    
    * button
    
    * dsfdfds
    
    * new
    
    * Blog card satrted
    
    * tek17
    
    * card finished
    
    * container added
    
    * add tek19
    
    * select added
    
    * card addwd
    
    * img
    
    * blog components
    
    * category finished
    
    * ddd
    
    * popular tag
    
    * blog filter added
    
    * search added
    
    * top categoryni saralashi ozgardi
    
    * top  category finished
    
    * font
    
    * img added
    
    * select added

--- src/pages/Blog/components/TopCategories.jsx
+++ src/pages/Blog/components/TopCategories.jsx
@@ -1,30 +1,52 @@
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
 
-const BlogTopCategories = ({ filterData }) => {
-  const arr = []
-  const obj = {}
+const BlogTopCategories = ({ data, setFilterData }) => {
+  const [currentCategory, setCurrentCategory] = useState('')
 
-  filterData.forEach((data) => {
-    arr.push(data.category)
+  useEffect(() => {
+    if (currentCategory == '') {
+      setFilterData([...data])
+    } else {
+      const newData = data.filter((d) => d.category == currentCategory)
+      setFilterData([...newData])
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentCategory])
+
+  const arrForData = []
+  const objForData = {}
+
+  // data ------------------------------------------------
+  data.forEach((data) => {
+    arrForData.push(data.category)
   })
 
-  for (let i = 0; i < arr.length; i++) {
-    if (obj[arr[i]]) {
-      obj[arr[i]] = obj[arr[i]] + 1
+  for (let i = 0; i < arrForData.length; i++) {
+    if (objForData[arrForData[i]]) {
+      objForData[arrForData[i]] = objForData[arrForData[i]] + 1
     } else {
-      obj[arr[i]] = 1
+      objForData[arrForData[i]] = 1
     }
   }
+  const dataKey = Object.entries(objForData)
 
-  const categorys = Object.entries(obj)
-
+  // ---------------------------------------------------
   return (
     <div>
       <h3 className="mb-[20px] font-bold">Top Categories</h3>
       <ul className="flex flex-col gap-4">
-        {categorys.map((category, idx) => {
+        {dataKey.map((category, idx) => {
           return (
-            <li key={idx} className="flex justify-between item-center pb-[16px ]">
+            <li
+              key={idx}
+              onClick={() => {
+                setCurrentCategory(currentCategory == category[0] ? '' : category[0])
+              }}
+              className={`flex justify-between item-center pb-[10px ] py-[3px] rounded cursor-pointer font-[450] hover:text-primary ${
+                category[0] == currentCategory ? 'text-primary bg-secondary ' : ''
+              }`}
+            >
               <p>{category[0]}</p>
               <p>({category[1]})</p>
             </li>
@@ -39,4 +61,6 @@ export default BlogTopCategories
 
 BlogTopCategories.propTypes = {
   filterData: PropTypes.array,
+  data: PropTypes.array,
+  setFilterData: PropTypes.any,
 }
