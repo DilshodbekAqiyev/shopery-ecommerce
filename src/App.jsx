8ad6d90e3ca301430cd15a5081242134b4e10a4c
Noila <noilaabdilamiyeva6467@gmail.com>
2023-11-08 09:34:00 +0500

    Dashboard Page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import {
   createBrowserRouter,
   RouterProvider,
   createRoutesFromElements,
-  Route
-} from 'react-router-dom'
+  Route,
+} from "react-router-dom";
 
 // Pages
-import Home from './pages/Home/Home'
-import Shop from './pages/Shop'
-import Blog from './pages/Blog/Blog'
-import ProductDetails from './pages/ProductDetails/ProductDetails'
-import Signup from './pages/Auth/Signup'
-import Signin from './pages/Auth/Signin'
-import PageNotFound from './components/common/PageNotFound'
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop";
+import Blog from "./pages/Blog/Blog";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Signup from "./pages/Auth/Signup";
+import Signin from "./pages/Auth/Signin";
+import PageNotFound from "./components/common/PageNotFound";
 
 // Layouts
-import RootLayout from './components/layouts/RootLayout'
+import RootLayout from "./components/layouts/RootLayout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import UserAddress from "./pages/Dashboard/components/UserAddress";
+import DashboardInfo from "./pages/Dashboard/components/DashboardInfo";
+import OrderDetail from "./pages/Dashboard/components/OrderDetail";
+import OrderHistory from "./pages/Dashboard/components/OrderHistory";
 
 export default function App() {
   const routes = createBrowserRouter(
@@ -27,11 +32,27 @@ export default function App() {
         <Route path="shop" element={<Shop />} />
         <Route path="sign-in" element={<Signup />} />
         <Route path="sign-up" element={<Signin />} />
+        {/* <Route pa
+        th="dashboard" element={<Dashboard />}>
 
+          <Route index  element={<DashboardInfo/>} />
+          <Route path="order-history" element = {<OrderHistory/>}>
+          <Route path="order-detail" element= {<OrderDetail></OrderDetail>}></Route>
+          </Route>
+          <Route path="wishlist" element={<UserAddress />} />
+        </Route> */}
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route index element={<DashboardInfo />} />
+          {/* <Route path="order-history" element={<OrderHistory  />}> */}
+          <Route path="order-history" element={<OrderDetail  />}>
+            <Route path="order-detail" index element={<OrderDetail></OrderDetail>} />
+          </Route>
+          <Route path="wishlist" element={<UserAddress />} />
+      </Route>
         <Route path="*" element={<PageNotFound />} />
       </Route>
     )
-  )
+  );
 
-  return <RouterProvider router={routes} />
+  return <RouterProvider router={routes} />;
 }
