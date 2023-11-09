e7a7e4f61d5bd870a52d73c97478bce41743dd11
Noila <noilaabdilamiyeva6467@gmail.com>
2023-11-09 22:50:33 +0500

    conflict fixed

--- src/pages/Dashboard/components/Profile.jsx
+++ src/pages/Dashboard/components/Profile.jsx
@@ -1,17 +1,21 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Profile() {
   return (
-    <div className= 'text-center flex  justify-center pt-[32px] items-center flex-col  border-solid border-2 border-gray100 rounded-[10px] mt-10  w-[536px] h-[310px] '>
-        <img src="../../../../public/assets/icons/user__profile__img.svg" alt="" />
-        <h4 className='mt-8 font-semibold text-[20px] leading-[30px] '>Dianne Russell</h4>
-        <p className='text-[#808080]  mb-[9px]'>Customer</p>
-        <Link to={'settings'}>
-        <button className='border-none  outline-none  text-[16px] font-semibold mb-[32px]   text-[#00B207] '>Edit Profile</button>
-        </Link>
+    <div className="text-center flex  justify-center pt-[32px] items-center flex-col  border-solid border-2 border-gray100 rounded-[10px] mt-10  w-[536px] h-[310px] ">
+     
+      <img src="/assets/icons/user__profile__img.svg" alt="" />
+      <h4 className="mt-8 font-semibold text-[20px] leading-[30px] ">
+        Dianne Russell
+      </h4>
+      <p className="text-[#808080]  mb-[9px]">Customer</p>
+      <Link to={'settings'}>
+        <button className="border-none  outline-none  text-[16px] font-semibold mb-[32px]   text-[#00B207] ">
+          Edit Profile
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
