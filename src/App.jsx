bd8bfb83c4d6706c3c7406aa4ef88fce49de4fb6
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-11-10 11:15:02 +0500

    finish add product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Navigate } from 'react-router-dom'
 
 // Pages
-import Home from "./pages/Home/Home";
-import Shop from "./pages/Shop";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-import Signup from "./pages/Auth/Signup";
-import Signin from "./pages/Auth/Signin";
-import UserAddress from "./pages/Dashboard/components/UserAddress";
-import DashboardInfo from "./pages/Dashboard/components/DashboardInfo";
-import OrderDetail from "./pages/Dashboard/components/OrderDetail";
-import Settings from "./pages/Dashboard/components/Settings";
+import Home from './pages/Home/Home'
+import Shop from './pages/Shop'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
+import Signup from './pages/Auth/Signup'
+import Signin from './pages/Auth/Signin'
+import UserAddress from './pages/Dashboard/components/UserAddress'
+import DashboardInfo from './pages/Dashboard/components/DashboardInfo'
+import OrderDetail from './pages/Dashboard/components/OrderDetail'
+import Settings from './pages/Dashboard/components/Settings'
 import About from './pages/About/About'
 import Checkout from './pages/Checkout/Checkout'
 import Wishlist from './pages/Wishlist/Wishlist'
@@ -32,7 +26,7 @@ import ShopProvider from './contexts/shop/ShopContext'
 import Index from './pages/Blog/Index'
 import BlogCards from './pages/Blog/components/BlogCards'
 import SingleBlog from './pages/Blog/Single Blog/SingleBlog'
-import PageNotFound from "./pages/PageNotFound";
+import PageNotFound from './pages/PageNotFound'
 
 export default function App() {
   const routes = createBrowserRouter(
@@ -57,12 +51,12 @@ export default function App() {
         <Route path="sign-up" element={<Signin />} />
         <Route path="dashboard" element={<Dashboard />}>
           <Route index element={<DashboardInfo />} />
-          
+
           <Route path="order-history" element={<OrderDetail />}>
             <Route path="order-detail" index element={<OrderDetail></OrderDetail>} />
           </Route>
           <Route path="wishlist" element={<UserAddress />} />
-          <Route path="settings" element={<Settings/>}/>
+          <Route path="settings" element={<Settings />} />
           <Route path="addProduct" element={<AddProduct />} />
         </Route>
         <Route path="about" element={<About />} />
@@ -73,7 +67,7 @@ export default function App() {
         <Route path="*" element={<Navigate to={'/PageNotFound'} />} />
       </Route>
     )
-  );
+  )
 
-  return <RouterProvider router={routes} />;
+  return <RouterProvider router={routes} />
 }
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,1 +1,38 @@
 export const CLOUDINARY_UPLOAD_NAME = 'shopery'
+
+export const initialProductState = {
+  id: new Date().getTime(),
+  name: '',
+  category: '',
+  discountPrice: null || '',
+  originalPrice: null,
+  discountPercentage: null,
+  status: '',
+  statusColor: '1A1A1A',
+  rating: 1,
+  available: 1,
+  littleDescription:
+    'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar.',
+  description: '',
+  images: {
+    src: '',
+    imageDescription: 'Image',
+    otherImages: [
+      {
+        id: 1,
+        src: '/assets/images/products/potato.png',
+        otherImageDescription: 'Picture of a large potato from the left',
+      },
+    ],
+  },
+  additionalInformation: [
+    {
+      title: 'Weight:',
+      description: '02',
+    },
+  ],
+  brand: '/assets/images/products/brands/farmary.svg',
+  tag: ['Vegetables'],
+  reviews: [],
+  access_token: localStorage.getItem('access_token'),
+}
