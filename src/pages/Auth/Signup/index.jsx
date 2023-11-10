7335da55dd5f6dceebb033dbd9bc4ece881ac669
Baxriddin Chorshanbiyev <baxriddin.dev@gmail.com>
2023-11-10 08:30:20 +0500

    SIGNUP

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 import { AiOutlineEye } from "react-icons/ai";
+
 import { Input } from "../../../components/ui/input";
+import { Checkbox } from "../../../components/ui/checkbox";
+import { Button } from "../../../components/ui/button";
 
 import Container from "../../../components/common/Container";
 
 export default function Signup() {
+  const createUser = () => {
+    alert('Error')
+  }
+
   return (
     <Container>
       <div className="w-full h-[100vh] flex items-center justify-center">
@@ -13,25 +21,31 @@ export default function Signup() {
           <div className="w-[100%] flex flex-col gap-[12px]">
             <div className="w-[100%] flex h-[49px] gap-2 items-center px-[14px] border rounded-md">
               <Input
-                className="border-none focus-visible:ring-0 p-0"
+                className="border-none focus-visible:ring-0 p-0 placeholder:text-gray-400"
                 type="text"
                 placeholder="Email" />
             </div>
             <div className="w-[100%] flex h-[49px] gap-2 items-center px-[14px] border rounded-md">
               <Input
-                className="border-none h-[100%] focus-visible:ring-0 focus:outline-none p-0"
+                className="border-none h-[100%] focus-visible:ring-0 focus:outline-none p-0 placeholder:text-gray-400"
                 type="password"
                 placeholder="Password" />
               <AiOutlineEye size={24} />
             </div>
             <div className="w-[100%] flex h-[49px] gap-2 items-center px-[14px] border rounded-md">
               <Input
-                className="border-none h-[100%] focus-visible:ring-0 focus:outline-none p-0"
+                className="border-none h-[100%] focus-visible:ring-0 focus:outline-none p-0 placeholder:text-gray-400"
                 type="password"
                 placeholder="Confirm Password" />
               <AiOutlineEye size={24} />
-            </div>7
+            </div>
+            <label className="flex items-center gap-1 cursor-pointer w-[55%] select-none">
+              <Checkbox />
+              <span className="text-gray-500">Accept all terms & Conditions</span>
+            </label>
           </div>
+          <Button onClick={createUser} className="w-full rounded-[30px]">Create Account</Button>
+          <p className="text-gray-500">Already have account? <Link to="/sign-in" className="text-black font-bold">Login</Link></p>
         </div>
       </div>
     </Container>
