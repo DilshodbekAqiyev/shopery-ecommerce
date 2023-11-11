877e73ce5f317a298c6dbe4796e333af1b32e4ee
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-11-11 21:46:24 +0500

    releted products added

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -5,28 +5,40 @@ import TabBar from './components/Tabs'
 import { useParams } from 'react-router-dom'
 import { instance } from '../../utils/apiRequest'
 import { useEffect, useState } from 'react'
+import ProductCard from '../../components/common/Cards/ProductCard'
 
 function ProductDetails() {
   const { productID } = useParams()
   const [foundProduct, setFoundProduct] = useState({})
+  const [sliceProduct, setSliceProduct] = useState([])
 
   console.log(foundProduct)
 
   useEffect(() => {
-    // eslint-disable-next-line no-extra-semi
     ;(async () => {
       const response = await instance.get(`products/${productID}`)
       setFoundProduct(response.data)
     })()
   }, [productID])
 
+  useEffect(() => {
+    ;(async () => {
+      const response = await instance.get(`products?_start=1&_end=5`)
+      console.log(response.data)
+      setSliceProduct(response.data)
+    })()
+  }, [])
+
   return (
     <div className="max-w-[1320px] mx-auto">
-      <>
-        <ModalDetails product={foundProduct} />
-        <TabBar product={foundProduct} />
-      </>
+      <ModalDetails product={foundProduct} />
+      <TabBar product={foundProduct} />
       <h1 className="text-heading05 font-[600] text-center mb-8">Related Products</h1>
+      <div className="flex justify-between">
+        {sliceProduct?.map((item) => {
+          return <ProductCard key={item.id} {...item} />
+        })}
+      </div>
     </div>
   )
 }
