a70ed7af79067f9a66bd54f78f4138e1c91bfe4c
Suhrob <135635990+SuhrobAbdumalikov@users.noreply.github.com>
2023-11-11 20:52:25 +0500

    Suxrob (#99)
    * added: typography and colors
    
    * prepared:Tabs
    
    * fix:Conflicts
    
    * fix:Conflicts
    
    * fix:bug
    
    * fix:bug
    
    * added:description Tab
    
    * added:description Tab
    
    * finished:Tabs
    
    * buttuns prepared started
    
    * prepared:all btn types and size
    
    * added:Fonts
    
    * fixed:tabs bugs
    
    * added:video,reducer,types and fixed:prettier
    
    * useState changed to useReducer
    
    * fixed:conflicts
    
    * added:Slider images
    
    * remove:smth
    
    * fixed: conflicts
    
    * fix:bug
    
    * smth added
    
    * finished:mypage ui finished
    
    * fixed:conflicts
    
    * added:smth
    
    * breadCumps finished
    
    * breadCump finished full
    
    * fix:conflicts
    
    * fix:breadCrump problem
    
    * fix:breadcump and my page
    
    * breadcump fixed
    
    * breadcump added to layout
    
    * home scroll fixed
    
    * product details continuea and db json changed
    
    * fixed:bugs
    
    * pull from main
    
    * details section drow with data
    
    * modal added
    
    * modal bug fixed
    
    * fix:conflicts
    
    * loop property added to swiper
    
    * fix:bug
    
    * fix:conflicts
    
    * started:wishlist post
    
    * fix:conflicts
    
    * try:post wishlist
    
    * fix:conflicts and url path
    
    * smth changed
    
    * wishlist:fucntion is finished only ui left
    
    * wishlist function finished only ui left

--- src/utils/api/AddToWishlist.jsx
+++ src/utils/api/AddToWishlist.jsx
@@ -1,17 +1,31 @@
 import PropTypes from 'prop-types'
 import { instance } from '../apiRequest'
 import { getUser } from '../utils'
-export const AddToWishlist = ({ id }) => {
-  // name, category, originalPrice, discountPrice, status, images
-
+export const AddToWishlist = (product) => {
+  const { id, name, category, originalPrice, discountPrice, status, images } = product
+  // console.log(id)
   getUser().then((user) => {
-    const request = instance.post(`users/${user.wishlist}/1`, {
-      id: id,
-      name: 'suhrob',
-      category: 'vegetables',
-    })
-    console.log(user.wishlist)
-    console.log(request)
+    const el = user.wishlist.find((wishitem) => wishitem.id === id)
+    if (!el) {
+      user.wishlist.push({
+        id,
+        name,
+        category,
+        discountPrice,
+        originalPrice,
+        status,
+        images,
+      })
+      const request = instance.patch(`users/1`, user)
+      console.log(request)
+      console.log('added: =>' + user)
+    } else {
+      const newData = user.wishlist.filter((item) => item.id !== el.id)
+      user.wishlist = newData
+      const request = instance.patch(`users/1`, user)
+      console.log(request)
+      console.log('deleted: =>' + user)
+    }
   })
 }
 AddToWishlist.propTypes = {
