aa440219500627559c67892e3321565a45bb39f9
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-11-12 23:35:19 +0500

    wishlist is drowen with data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, N
 
 // Pages
 import Home from './pages/Home/Home'
-
 import Shop from './pages/Shop'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
 import Signup from './pages/Auth/Signup'
@@ -10,6 +9,7 @@ import Signin from './pages/Auth/Signin'
 import PageNotFound from './pages/PageNotFound'
 import About from './pages/About/About'
 
+//components
 import BlogCards from './pages/Blog/components/BlogCards'
 import SingleBlog from './pages/Blog/Single Blog/SingleBlog'
 import DashboardInfo from './pages/Dashboard/components/DashboardInfo'
@@ -21,6 +21,7 @@ import Dashboard from './pages/Dashboard/Dashboard'
 import RootLayout from './components/layouts/RootLayout'
 import Index from './pages/Blog/Index'
 import ShopProvider from './contexts/shop/ShopContext'
+import Wishlist from './pages/Wishlist/Wishlist'
 
 export default function App() {
   const routes = createBrowserRouter(
@@ -60,6 +61,7 @@ export default function App() {
           </Route>
           <Route path="wishlist" element={<UserAddress />} />
         </Route>
+        <Route path="wishlist" element={<Wishlist />} />
         <Route path="blog/:CardID" element={<SingleBlog />} />
         <Route path="shop" element={<Shop />} />
         <Route path="shop/:productID" element={<ProductDetails />} />
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,35 +1,20 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { instance } from '../../utils/apiRequest'
 import SocialMediaIcons from '../../components/common/SocialMediaIcons'
-// import { instance } from '../../utils/apiRequest'
-// import { useEffect, useState } from 'react'
 
 export default function Wishlist() {
-  const data = [
-    {
-      imgUrl: 'https://seed2plant.in/cdn/shop/products/greencapsicum.jpg?v=1606107907',
-      title: 'Green Capsicum',
-      price: '$14.99',
-      spanPrice: '$20.99',
-      stock: true,
-      stockTitle: 'In Stock',
-      btn: 'Add to Cart',
-    },
-    {
-      imgUrl: 'https://www.veggycation.com.au/siteassets/veggycationvegetable/cabbage-chinese.jpg',
-      title: 'Chinese Cabbage',
-      price: '$45.00',
-      stock: true,
-      stockTitle: 'In Stock',
-      btn: 'Add to Cart',
-    },
-    {
-      imgUrl: 'https://exoticfruitbox.com/wp-content/uploads/2015/10/mango.jpg',
-      title: 'Fresh Sujapuri Mango',
-      price: '$09.00',
-      stock: false,
-      stockTitle: 'Out of Stock',
-      btn: 'Add to Cart',
-    },
-  ]
+  const [wishlistData, setWishlistData] = useState([])
+
+  const getWishData = async () => {
+    const data = await instance.get('users/1')
+    setWishlistData(data.data.wishlist)
+    console.log(data.data.wishlist)
+  }
+
+  useEffect(() => {
+    getWishData()
+  }, [])
   return (
     <div className="bg-[#F5F5F5]">
       <div className="container pt-[110px]  pb-[80px]">
@@ -40,34 +25,33 @@ export default function Wishlist() {
               <h3 className="text-[#808080] text-[14px]">PRICE</h3>
               <h3 className="text-[#808080] text-[14px]">STOCK STATUS</h3>
             </div>
-            {/* <h3></h3> */}
           </div>
           <hr className="bg-[#EDEDED] h-[1px] mb-[30px]" />
           <div className="pl-[24px] pr-[24px] pb-[24px]">
-            {data.map((data, key) => {
+            {wishlistData.map((data, index) => {
               return (
-                <div className="flex mt-[24px] items-center justify-between w-[1272px] text-left" key={key}>
+                <div className="flex mt-[24px] items-center justify-between w-[1272px] text-left" key={index}>
                   <div className="flex gap-x-[20px] items-center">
-                    <img src={data.imgUrl} alt={data.title} className="w-[100px] h-[100px]" />
-                    <h4 className="text-[16px] text-[#1A1A1A]">{data.title}</h4>
+                    <img src={data.images.src} alt={data.images.imageDescription} className="w-[100px] h-[100px]" />
+                    <h4 className="text-[16px] text-[#1A1A1A]">{data.name}</h4>
                   </div>
-                  {data.spanPrice ? (
+                  {data.originalPrice ? (
                     <div className="flex">
-                      <h3 className="text-[#1A1A1A] text-[16px] text-left">{data.price}</h3>
-                      <del className="text-[#999] text-[16px]">{data.spanPrice}</del>
+                      <h3 className="text-[#1A1A1A] text-[16px] text-left">{`$${data.discountPrice}`}</h3>
+                      <del className="text-[#999] text-[16px] ml-1">{`$${data.originalPrice}`}</del>
                     </div>
                   ) : (
-                    <h3 className="text-[16px] text-left">{data.price} </h3>
+                    <h3 className="text-[16px] text-left">{data.discountPrice} </h3>
                   )}
                   <div>
-                    {data.stock ? (
+                    {!data.status ? (
                       <div className="flex gap-x-[136px] items-center">
                         <h3 className="text-[#2C742F] text-[14px] bg-[#CAE8CC] text-center px-[8px] py-[4px]">
-                          {data.stockTitle}
+                          In Stock
                         </h3>
                         <div className="flex gap-x-[24px] items-center">
                           <button className="bg-[#00B207] rounded-[43px] text-[white] px-[32px] py-[15px] hover:bg-[#19BA1F] transition">
-                            {data.btn}
+                            Add to Cart
                           </button>
                           <button className="w-[24px] h-[24px] p-[2px] rounded-[50%] border-gray-500">
                             <i className="fa-regular fa-circle-xmark text-[gray] scale-[1.2]"></i>
@@ -76,10 +60,10 @@ export default function Wishlist() {
                       </div>
                     ) : (
                       <div className="flex gap-x-[106px] items-center">
-                        <h3 className="text-[#EA4B48] text-[14px] bg-[#F3D3D2] px-[8px] py-[4px]">{data.stockTitle}</h3>
+                        <h3 className="text-[#EA4B48] text-[14px] bg-[#F3D3D2] px-[8px] py-[4px]">{data.status}</h3>
                         <div className="flex gap-x-[24px] items-center">
                           <button className="bg-[#F2F2F2] rounded-[43px] text-[#B3B3B3] px-[32px] py-[15px] cursor-not-allowed">
-                            {data.btn}
+                            Add to Cart
                           </button>
                           <button className="w-[24px] h-[24px] rounded-[50%] border-gray-500 ">
                             <i className="fa-regular fa-circle-xmark text-[gray] scale-[1.2]"></i>
